05e13dd9382fb16f6b0d3c901cbc2ded0730f23f
Jimmy Batuhan <jimmy.batuhan@ph.arbcalls.com>
2021-04-12 21:01:22 +0800

    fix error message not showing

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -6,13 +6,24 @@ use App\Shopify\Facades\ShopifyAdmin;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\ViewErrorBag;
 use Illuminate\View\View;
 
 class CustomerController extends Controller
 {
+    private $errors = [];
+    private $inputs = [];
+
     public function register(): View
     {
-        return view('register');
+        $view_data = [];
+
+        if ($this->errors && $this->inputs) {
+            $view_data['errors'] = $this->errors;
+            $view_data['input'] = $this->inputs;
+        }
+
+        return view('register', $view_data);
     }
 
     public function processRegistration(Request $request)
@@ -26,10 +37,11 @@ class CustomerController extends Controller
             'confirm_password' => 'required',
         ]);
 
-        if($validator->fails()) {
-
-            dd('failed validation');
-//            return Redirect::back()->withErrors($validator)->withInput();
+        if ($validator->fails()) {
+            $this->errors = (new ViewErrorBag())->put('default', $validator->getMessageBag());
+            $this->inputs = $request->all();
+            return $this->register();
+            // return Redirect::back()->withErrors($validator)->withInput();
         }
     }
 }

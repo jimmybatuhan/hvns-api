e41d652063a038b3475c3588cf38ff5c3e300693
Daryl Dionisio <>
2021-04-25 10:33:31 +0800

    added a env checker on the AppServiceProvider and update .env.example file

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,5 +28,24 @@ class AppServiceProvider extends ServiceProvider
         if (! App::environment('local')) {
             URL::forceScheme('https');
         }
+
+        $this->checkConfigs();
+
+    }
+
+    private function checkConfigs(){
+
+        throw_if(empty(config('app.shopify_api_version')), 'RuntimeException', 'Shopify API Version is not set on the env file.');
+        throw_if(empty(config('app.shopify_store_url')), 'RuntimeException', 'Shopify Store URL is not set on the env file.');
+        throw_if(empty(config('app.shopify_access_token')), 'RuntimeException', 'Shopify Store URL is not set on the env file.');
+        throw_if(empty(config('app.shopify_app_secret')), 'RuntimeException', 'Shopify App Secret is not set on the env file.');
+
+        throw_if(empty(config('app.zap_api_version')), 'RuntimeException', 'ZAP API Version is not set on the env file');
+        throw_if(empty(config('app.zap_access_token')), 'RuntimeException', 'ZAP Access Token is not set on the env file.');
+        throw_if(empty(config('app.zap_api_endpoint')), 'RuntimeException', 'ZAP API Endpoint is not set on the env file.');
+        throw_if(empty(config('app.zap_branch_id')), 'RuntimeException', 'ZAP Branch ID is not set on the env file.');
+        throw_if(empty(config('app.zap_merchant_id')), 'RsuntimeException', 'ZAP Merchant ID is not set on the env file.');
+        throw_if(empty(config('app.zap_location_id')), 'RuntimeException', 'ZAP Location ID is not set on the env file.');
+
     }
 }

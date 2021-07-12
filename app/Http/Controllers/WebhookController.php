18f8c3cf812851198db4d3e213297f36d2fb1f3c
Jimmy Batuhan <jimmybatuhan@gmail.com>
2021-07-12 12:18:36 +0800

    update on cancelled behavior

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -24,10 +24,6 @@ class WebhookController extends Controller
         $status = Response::HTTP_OK;
         $body = $request->all();
         $order_id = $body['id'];
-        $metafields = collect([
-            'order_id' => $order_id,
-            'sub_total' => floatval($body['subtotal_price']),
-        ]);
 
         if (Arr::exists($body, 'customer')) {
             $customer = $body['customer'];
@@ -135,61 +131,148 @@ class WebhookController extends Controller
         $body = $request->all();
         $order_id = $body['id'];
         $line_items = collect($body['line_items']);
+        $is_cancelled = !is_null($body['cancelled_at']);
 
-        $order_metafields = ShopifyAdmin::fetchMetafield($order_id, ShopifyConstants::ORDER_RESOURCE);
+        if ($is_cancelled) {
+            abort(200);
+        } else {
+            $order_metafields = ShopifyAdmin::fetchMetafield($order_id, ShopifyConstants::ORDER_RESOURCE);
 
-        $points_earned_metafield = $order_metafields->getPointsEarnedMetafieldId();
-        $points_earned = $order_metafields->getPointsEarnedMetafield();
+            $points_earned_metafield = $order_metafields->getPointsEarnedMetafieldId();
+            $points_earned = $order_metafields->getPointsEarnedMetafield();
 
-        $line_item_points_metafield = $order_metafields->getLineItemPointsMetafieldId();
-        $line_item_points = $order_metafields->getLineItemPointsMetafield();
-        $line_item_points_original_count = sizeof($line_item_points);
+            $line_item_points_metafield = $order_metafields->getLineItemPointsMetafieldId();
+            $line_item_points = $order_metafields->getLineItemPointsMetafield();
+            $line_item_points_original_count = sizeof($line_item_points);
 
-        /**
-         * if the order is not cancelled and has a points_to_earn metafield
-         * recalculate the points to be earn
-         */
-        if ($points_earned_metafield) {
-            $total_points_collection = $line_items->map(function (array $item) use (&$line_item_points) {
-                $result = ShopPromo::calculatePointsToEarn($item, $line_item_points);
+            /**
+             * if the order is not cancelled and has a points_to_earn metafield
+             * recalculate the points to be earn
+             */
+            if ($points_earned_metafield) {
+                $total_points_collection = $line_items->map(function (array $item) use (&$line_item_points) {
+                    $result = ShopPromo::calculatePointsToEarn($item, $line_item_points);
 
-                if (! array_key_exists($result['id'], $line_item_points) ) {
-                    $line_item_points[$result['id']] = [
-                        "id" => $result['id'],
-                        "reward_type" => $result['reward_type'],
-                        "reward_amount" => $result['reward_amount'],
-                        "points_to_earn" => $result['points_to_earn'],
-                    ];
+                    if (! array_key_exists($result['id'], $line_item_points) ) {
+                        $line_item_points[$result['id']] = [
+                            "id" => $result['id'],
+                            "reward_type" => $result['reward_type'],
+                            "reward_amount" => $result['reward_amount'],
+                            "points_to_earn" => $result['points_to_earn'],
+                        ];
+                    }
+
+                    return $result;
+                });
+
+                $line_item_points_new_count = sizeof($line_item_points);
+
+                $total_points_to_earn = $total_points_collection->sum('points_to_credit');
+                $total_subtotal_amount = $total_points_collection->sum('subtotal_amount');
+                $line_item_points_collection = collect($line_item_points);
+
+                if ($total_points_to_earn != $points_earned) {
+
+                    if ($points_earned > 0) {
+                        $this->customerDeductZAPPoints($body, $points_earned);
+                    }
+
+                    if (floatval($total_subtotal_amount) >= ShopifyConstants::MINIMUM_SUBTOTAL_TO_EARN) {
+                        $this->customerRewardPoints($body, $total_points_to_earn);
+                    }
+
+                    ShopifyAdmin::updateMetafieldById($points_earned_metafield, $total_points_to_earn);
                 }
 
-                return $result;
-            });
-
-            $line_item_points_new_count = sizeof($line_item_points);
-
-            $total_points_to_earn = $total_points_collection->sum('points_to_credit');
-            $total_subtotal_amount = $total_points_collection->sum('subtotal_amount');
-            $line_item_points_collection = collect($line_item_points);
-
-            if ($total_points_to_earn != $points_earned) {
-
-                if ($points_earned > 0) {
-                    $this->customerDeductZAPPoints($body, $points_earned);
+                if ($line_item_points_new_count != $line_item_points_original_count) {
+                    ShopifyAdmin::updateMetafieldById($line_item_points_metafield, $line_item_points_collection->toJson());
                 }
-
-                if (floatval($total_subtotal_amount) >= ShopifyConstants::MINIMUM_SUBTOTAL_TO_EARN) {
-                    $this->customerRewardPoints($body, $total_points_to_earn);
-                }
-
-                ShopifyAdmin::updateMetafieldById($points_earned_metafield, $total_points_to_earn);
             }
 
-            if ($line_item_points_new_count != $line_item_points_original_count) {
-                ShopifyAdmin::updateMetafieldById($line_item_points_metafield, $line_item_points_collection->toJson());
+            return response()->json(['success' => true], Response::HTTP_OK);
+        }
+    }
+
+    public function onOrderCanceled(Request $request): JsonResponse
+    {
+        $success = true;
+        $status = Response::HTTP_OK;
+        $body = $request->all();
+        $order_id = $body['id'];
+        $customer = $body['customer'];
+        $dicount_codes = $body['discount_codes'];
+        $line_items = collect($body['line_items']);
+
+        if (Arr::exists($body, 'customer')) {
+            $customer = $body['customer'];
+            $customer_id = $customer['id'];
+            $mobile = substr($customer['phone'], 1);
+            $order_metafields = ShopifyAdmin::fetchMetafield($order_id, ShopifyConstants::ORDER_RESOURCE);
+            $customer_metafields = ShopifyAdmin::fetchMetafield($customer_id, ShopifyConstants::CUSTOMER_RESOURCE);
+            $customer_member_id = $customer_metafields->ZAPMemberId(ShopifyConstants::METAFIELD_INDEX_VALUE);
+            $zap_discount = collect($dicount_codes)
+                ->filter( fn ($discount) => $discount['code'] === ZAPConstants::DISCOUNT_PREFIX . $customer_id)
+                ->first();
+
+            // if customer has a member id from ZAP, this means we should do the next process
+            // else ignore the event.
+
+            $has_some_fulfilled = $line_items->filter(fn (array $item) => $item['fulfillment_status'] === 'fulfilled')->count() > 0;
+
+            if ($customer_member_id) {
+                $order_transaction_list = $order_metafields->ZAPTransactions();
+                $transactions = collect([]);
+                $transactions_metafield_id = null;
+
+                // If transaction list is not empty, decode else create a an empty collection
+                if ($order_transaction_list) {
+                    $transactions_metafield_id = $order_transaction_list[ShopifyConstants::METAFIELD_INDEX_ID];
+                    $transactions = collect(json_decode($order_transaction_list[ShopifyConstants::METAFIELD_INDEX_VALUE], true));
+                }
+
+                // if the customer used their zap points as a discount, if order is cancelled, we will return the points
+                if ($zap_discount && !$has_some_fulfilled) {
+                    $points_used = $zap_discount['amount'];
+                    $use_points_response = ZAP::addPoints($points_used, $mobile);
+
+                    if ($use_points_response->ok()) {
+                        $use_points_response_body = $use_points_response->collect();
+                        $zap_transaction_reference_no = $use_points_response_body['data']['refNo'];
+                        $zap_return_point_transaction = [
+                            ZAPConstants::TRANSACTION_REFERENCE_KEY => $zap_transaction_reference_no,
+                            ZAPConstants::TRANSACTION_POINTS_KEY => $points_used,
+                            ZAPConstants::TRANSACTION_STATUS_KEY => ZAPConstants::RETURNED_POINT_STATUS,
+                            'fulfilled_at' => Carbon::now()->toIso8601String(),
+                        ];
+
+                        // Add this new transaction to the collection
+                        $transactions->push($zap_return_point_transaction);
+
+                        // Add or update the collection of transaction in the Order's metafield
+                        $this->createOrUpdateTransactionMetafield(
+                            $order_id,
+                            $order_metafields,
+                            $zap_return_point_transaction,
+                            $transactions_metafield_id,
+                            $transactions
+                        );
+
+                    } else {
+                        Log::critical("failed to add used points of cancelled order #{$order_id}", [
+                            'discount_code' => $zap_discount['code'],
+                            'amount' => $points_used,
+                            'customer_id' => $customer_id,
+                        ]);
+                    }
+                }
+            } else {
+                Log::warning("order #{$order_id} has been cancelled without a zap member information");
             }
+        } else {
+            Log::warning("order #{$order_id} has been cancelled without a customer information");
         }
 
-        return response()->json(['success' => true], Response::HTTP_OK);
+        return response()->json(['success' => $success], $status);
     }
 
     private function createOrUpdateCustomerBalanceMetafield(

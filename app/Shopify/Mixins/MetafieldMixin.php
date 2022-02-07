77ee745cbcdf8d7cc05c11f48025d5a33372e1cf
Jimmy <jimmybatuhan@gmail.com>
2022-02-07 18:45:07 +0800

    updated rest discount code

--- app/Shopify/Mixins/MetafieldMixin.php
+++ app/Shopify/Mixins/MetafieldMixin.php
@@ -136,11 +136,11 @@ class MetafieldMixin
     public function ActiveDiscountCode(): Closure
     {
         return function () {
-            return @json_decode($this->metafield(
+            return $this->metafield(
                 ZAPConstants::MEMBER_NAMESPACE,
                 "last_active_discount",
                 ShopifyConstants::METAFIELD_INDEX_VALUE,
-            ));
+            );
         };
     }
 

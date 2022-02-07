0452bfe0dee7211df1b0327e07b946edad64aa44
Jimmy <jimmybatuhan@gmail.com>
2022-02-07 18:48:59 +0800

    updated function

--- app/Http/Controllers/DiscountController.php
+++ app/Http/Controllers/DiscountController.php
@@ -209,7 +209,7 @@ class DiscountController extends Controller
         try {
             $metafields = ShopifyAdmin::fetchMetafield($shopify_customer_id, ShopifyConstants::CUSTOMER_RESOURCE);
             $active_discount_code_id = $metafields->ActiveDiscountCodeId();
-            $current_active_discount = collect($metafields->ActiveDiscountCode());
+            $current_active_discount = $metafields->ActiveDiscountCode();
 
             throw new Exception(json_encode(["a" => $current_active_discount]));
 
